1b7bc9ccd0573d4aef599f4b03e33636bde09653
Alden Bradford <aldenbradford@gmail.com>
2021-07-14 20:39:09 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,83 @@
+import formulas from "./modules/formulas.js";
+
+window.onload = function() {
+	const config = document.getElementById("config");
+	const exercise_display = document.getElementById("exercise_display");
+	const guess = document.getElementById("guess");
+	const generate_button = document.getElementById("generate");
+	const feedback = document.getElementById("feedback");
+	const formula_selections = document.getElementById("formula_selections");
+	
+	for(const [name, formula] of Object.entries(formulas)){
+		const div = document.createElement("div");
+		div.classList.add("formula");
+		
+		const formula_box = document.createElement("input");
+		formula_box.setAttribute("type", "checkbox");
+		formula_box.setAttribute("name", name);
+		formula_box.checked = true;
+		div.appendChild(formula_box);
+		
+		const formula_label = document.createElement("label");
+		formula_label.setAttribute("for", name);
+		formula_label.innerText = formula.text;
+		div.appendChild(formula_label);
+		
+		formula_selections.appendChild(div);	
+	}
+	
+	let formula_pool;
+	let close_threshold = 2;
+	let exact_threshold = 0;
+	get_config();
+	config.addEventListener("input", get_config);
+	
+	guess.form.addEventListener("submit", check_answer);
+	
+	let exercise;
+	generate_button.addEventListener("click", generate);
+	generate();
+	
+	function get_config(){
+		const data = new FormData(config);
+		formula_pool = (
+			Array.from(
+				data.entries(),
+				([name]) => name
+			)
+			.filter(name => (name in formulas))
+			.map(name => formulas[name])
+		);
+		close_threshold = data.get("closeness");
+		exact_threshold = data.get("exactness");
+	};
+	
+	function check_answer(event){
+		event.preventDefault();
+		const entry = Number(guess.value);
+		const distance = Math.abs(entry-exercise.answer);
+		if(distance <= exact_threshold){
+			feedback.innerText = "Correct!";
+		} else if (distance <= close_threshold){
+			feedback.innerText = "So close!";
+		} else {
+			feedback.innerText = "Guess again!";
+		}
+	};
+	
+	function generate(){
+		if(formula_pool.length < 1){
+			exercise_display.innerText = "Please select at least one formula";
+			return;
+		}
+		let formula = randomChoice(formula_pool);
+		exercise = formula.generate();
+		exercise_display.innerText = exercise.text;
+		feedback.innerText = "Type your guess above.";
+		MathJax.typeset();
+	};
+};
+
+function randomChoice(arr) {
+    return arr[Math.floor(arr.length * Math.random())];
+};
---
+++ modules/formulas.js
@@ -0,0 +1,67 @@
+function randomTriplet(){
+	return Math.floor(100+900*Math.random());
+};
+
+function getTriplets(n){
+	return Array(n).fill(null).map(() => randomTriplet());
+};
+
+function getDummy(n){
+	const base = [123, 456, 789];
+	return Array(n).fill(null).map((x,i) => base[i]);
+};
+
+function mantissa(x){
+	return Math.round(x*10**(2-Math.floor(Math.log10(x))));
+};
+
+function match_template(str, arr){
+	let result = str;
+	for(const [i, x] of arr.entries()){
+		result = result.replace('@'+(i+1), x);
+	}
+	return result;
+}
+
+function make_formula({func, template}){
+	function generate(){
+		const args = getTriplets(func.length);
+		const text = match_template(template, args);
+		const answer = mantissa(func(...args));
+		return {text, answer};
+	}
+	const text = match_template(template, getDummy(func.length));
+	return {text, generate};
+};
+
+const multiplication = make_formula({
+	template: "\\( @1 \\times @2 \\)",
+	func: ((a, b) => a*b)
+});
+
+const division = make_formula({
+	template: "\\( \\dfrac{ @1 }{ @2 } \\)",
+	func: ((a, b) => a/b)
+});
+
+const mul_mul_div = make_formula({
+	template: "\\( \\dfrac{ @1 \\times @2 }{ @3 } \\)",
+	func: ((a, b, c) => a*b/c)
+});
+
+const mul_div_div = make_formula({
+	template: "\\( \\dfrac{ @1 }{ @2 \\times @3 } \\)",
+	func: ((a, b, c) => a/(b*c))
+});
+
+const circle_area_from_diameter = make_formula({
+	template: "\\( \\pi \\dfrac{ @1 ^2 }{ 4 } \\)",
+	func: (d => Math.pi * d**2 /4)
+});
+
+const sqrt_of_product = make_formula({
+	template: "\\( \\sqrt{ @1 \\times @2 } \\)",
+	func: ((a, b) => Math.sqrt(a*b))
+});
+
+export default {multiplication, division, mul_mul_div, mul_div_div, circle_area_from_diameter, sqrt_of_product};
